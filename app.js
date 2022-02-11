6d032fc3aa6cbecf456417a29d62b3e3e0733ed0
luciano393 <luciano39340268@gmail.com>
2022-02-11 17:17:43 -0300

    calculadora IMC

---
+++ app.js
@@ -0,0 +1,20 @@
+const calcular = document.getElementById("btn")
+
+function calculadora() {
+    const nombre = document.getElementById("nombre").value;
+    const estatura = document.getElementById("estatura").value;
+    const peso = document.getElementById("peso").value;
+    const resultado = document.getElementById("resultado");
+    
+    const IMC = peso / Math.pow(estatura, 2);
+
+    if (IMC < 20) {
+        resultado.textContent = `${nombre}, tenés bajo peso.`;
+    } else if ( IMC > 25) {
+        resultado.textContent = `${nombre}, tenés sobre peso.`;
+    } else if ( IMC < 25 && IMC > 20) {
+        resultado.textContent = `${nombre}, tenés peso ideal.`;
+    }
+}
+
+calcular.addEventListener("click", calculadora);
