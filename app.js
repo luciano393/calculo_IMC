aa7c6de9826aab67e3ac1842dbcca00e121609a2
luciano393 <luciano39340268@gmail.com>
2022-02-11 18:55:08 -0300

    refactorizado

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const calcular = document.getElementById("btn")
+const d = document
+const calcular = d.getElementById("btn")
 
 const calculadora = () => {
-    const nombre = document.getElementById("nombre").value;
-    const estatura = document.getElementById("estatura").value;
-    const peso = document.getElementById("peso").value;
-    const resultado = document.getElementById("resultado");
+    const nombre = d.getElementById("nombre").value;
+    const estatura = d.getElementById("estatura").value;
+    const peso = d.getElementById("peso").value;
+    const resultado = d.getElementById("resultado");
     
     const IMC = peso / Math.pow(estatura, 2);
 
-    if (IMC < 20) {
-        resultado.textContent = `${nombre}, tenés bajo peso.`;
-    } else if ( IMC > 25) {
-        resultado.textContent = `${nombre}, tenés sobre peso.`;
-    } else if ( IMC < 25 && IMC > 20) {
-        resultado.textContent = `${nombre}, tenés peso ideal.`;
-    }
+    IMC < 20 
+    ? resultado.textContent = `${nombre}, tenés bajo peso.` 
+    : IMC > 25 ? resultado.textContent = `${nombre}, tenés sobre peso.` 
+    : resultado.textContent = `${nombre}, tenés peso ideal.`;
 }
 
 calcular.addEventListener("click", calculadora);
